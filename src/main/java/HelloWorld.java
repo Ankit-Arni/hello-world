ccfa86b25352bc0ffebd2fee199e0937d5861bb8
Ankit Arni <ankit.arni.1995@gmail.com>
2017-07-18 10:02:13 +0530

    Initial commit of hello-world to repo

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,31 @@
+/**
+ * Created by ankit.manoj on 18/7/2017.
+ */
+public class HelloWorld
+{
+
+    private String name = "";
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getMessage()
+    {
+        if (name == "")
+        {
+            return "Hello!";
+        }
+        else
+        {
+            return "Hello " + name + "!";
+        }
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+}
